40dedc45f6d6a31befdeacd31dce4617b371a47a
Geoff S <122327227+Deryaus@users.noreply.github.com>
2025-01-21 16:11:49 -0500

    Initial
    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+"""
+Description:
+ Main program for Fleming kiosk webpage
+
+Usage:
+ python kiosk.py
+
+Parameters:
+ None
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Routes
+
+@app.route('/')
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html') 
+
+if __name__ == '__main__':
+    app.run(debug=True) 
