1f6c82dd6f61c131fe1bd4a834d402c0d8efa42a
Geoff S <122327227+Deryaus@users.noreply.github.com>
2025-02-04 14:03:43 -0500

    Delete helloworld.py

--- helloworld.py
+++
@@ -1,8 +0,0 @@
-def helloworld(userInput):
-    output = "Hello World " + userInput + "! This is a test."
-    return output
-
-def testaudio():
-    userInput = "Test"
-    output = "Hello World! This is a test."
-    return userInput, output
